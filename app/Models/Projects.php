8bd769311e8130a04c0272d572cb297fc474f7ed
Qiu Chen <51996408+xXQiuChenXx@users.noreply.github.com>
2024-10-18 10:35:30 +0800

    Update Projects.php

--- app/Models/Projects.php
+++ app/Models/Projects.php
@@ -9,4 +9,9 @@ class Projects extends Model
 {
     /** @use HasFactory<\Database\Factories\ProjectsFactory> */
     use HasFactory;
+
+    public function tasks()
+    {
+        return $this->hasMany(Tasks::class);
+    }
 }
