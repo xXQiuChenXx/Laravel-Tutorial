7a443700d08c935db905fb53403e9c986475ace3
Qiu Chen <51996408+xXQiuChenXx@users.noreply.github.com>
2024-10-25 07:10:07 +0800

    data table

--- resources/js/Pages/Project/Index.tsx
+++ resources/js/Pages/Project/Index.tsx
@@ -1,3 +1,4 @@
+import { DataTableDemo } from "@/Components/DataTable";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import { Head } from "@inertiajs/react";
 
@@ -27,15 +28,8 @@ export default function Index({ projects }: Props) {
     >
       <Head title="Projects" />
 
-      <div className="py-12">
-        <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
-          <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg dark:bg-gray-800">
-            <div className="p-6 text-gray-900 dark:text-gray-100">
-              <pre>{JSON.stringify(projects, undefined, 2)}</pre>
-            </div>
-          </div>
-        </div>
-      </div>
+      <DataTableDemo />
+      
     </AuthenticatedLayout>
   );
 }
