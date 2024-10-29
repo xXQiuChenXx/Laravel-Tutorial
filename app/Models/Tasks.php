31ab05c3b0c4689fedab19d2e895fee637644906
Qiu Chen <51996408+xXQiuChenXx@users.noreply.github.com>
2024-10-29 10:27:13 +0800

    Update Tasks.php

--- app/Models/Tasks.php
+++ app/Models/Tasks.php
@@ -4,9 +4,40 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Tasks extends Model
 {
     /** @use HasFactory<\Database\Factories\TasksFactory> */
     use HasFactory;
+
+    public function projects(): BelongsTo
+    {
+        return $this->belongsTo(Projects::class);
+    }
+
+    public function assignedUser(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'assigned_user_id');
+    }
+
+    /**
+     * Get the createdBy that owns the Tasks
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function createdBy(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+
+    /**
+     * Get the updatedBy that owns the Tasks
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function updatedBy(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'updated_by');
+    }
 }
