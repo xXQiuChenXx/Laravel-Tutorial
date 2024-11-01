4586c5efab26fe05a053b7029bc03c2ef8a5fadc
Qiu Chen <51996408+xXQiuChenXx@users.noreply.github.com>
2024-11-02 06:24:15 +0800

    Update Show.tsx

--- resources/js/Pages/Projects/Show.tsx
+++ resources/js/Pages/Projects/Show.tsx
@@ -24,21 +24,60 @@ const Show = ({
               alt="image"
               className="w-full h-64 object-cover"
             />
-            <div className="p-6 text-gray-900 dark:text-gray-100"></div>
-            <div className="grid gap-1 grid-cols-2">
+            <div className="p-6 text-gray-900 dark:text-gray-100">
+              <div className="grid gap-1 grid-cols-2">
+                <div>
+                  <div>
+                    <label htmlFor="" className="font-bold text-lg">
+                      Project ID
+                    </label>
+                    <p className="mt-1">{data.id}</p>
+                  </div>
+                  <div>
+                    <label htmlFor="" className="font-bold text-lg">
+                      Project Name
+                    </label>
+                    <p className="mt-1">{data.name}</p>
+                  </div>
+                  <div>
+                    <label htmlFor="" className="font-bold text-lg">
+                      Project Status
+                    </label>
+                    <p className="mt-1">{data.status}</p>
+                  </div>
+                  <div>
+                    <label htmlFor="" className="font-bold text-lg">
+                      Created By
+                    </label>
+                    <p className="mt-1">{data.created_by?.name}</p>
+                  </div>
+                </div>
+                <div>
+                  <div>
+                    <label htmlFor="" className="font-bold text-lg">
+                      Due Date
+                    </label>
+                    <p className="mt-1">{data.due_date}</p>
+                  </div>
+                  <div>
+                    <label htmlFor="" className="font-bold text-lg">
+                      Create Date
+                    </label>
+                    <p className="mt-1">{data.created_at}</p>
+                  </div>
+                  <div>
+                    <label htmlFor="" className="font-bold text-lg">
+                      Updated By
+                    </label>
+                    <p className="mt-1">{data.updated_by.name}</p>
+                  </div>
+                </div>
+              </div>
               <div>
-                <div>
-                  <label htmlFor="" className="font-bold text-lg">
-                    Project ID
-                  </label>
-                  <p className="mt-1">{data.id}</p>
-                </div>
-                <div>
-                  <label htmlFor="" className="font-bold text-lg">
-                    Project Name
-                  </label>
-                  <p className="mt-1">{data.name}</p>
-                </div>
+                <label htmlFor="" className="font-bold text-lg">
+                  Project Description
+                </label>
+                <p className="mt-1">{data.description}</p>
               </div>
             </div>
           </div>
