1db9feb39a2883f8dd08416c536cce23543f7b39
Qiu Chen <51996408+xXQiuChenXx@users.noreply.github.com>
2024-11-11 21:08:36 +0800

    Create TextAreaInput.tsx

---
+++ resources/js/Components/TextAreaInput.tsx
@@ -0,0 +1,42 @@
+import {
+  forwardRef,
+  InputHTMLAttributes,
+  useEffect,
+  useImperativeHandle,
+  useRef,
+} from "react";
+
+export default forwardRef(function TextAreaInput(
+  {
+    className = "",
+    isFocused = false,
+    children,
+    ...props
+  }: InputHTMLAttributes<HTMLTextAreaElement> & { isFocused?: boolean },
+  ref
+) {
+  const localRef = useRef<HTMLTextAreaElement>(null);
+
+  useImperativeHandle(ref, () => ({
+    focus: () => localRef.current?.focus(),
+  }));
+
+  useEffect(() => {
+    if (isFocused) {
+      localRef.current?.focus();
+    }
+  }, [isFocused]);
+
+  return (
+    <textarea
+      {...props}
+      className={
+        "rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 dark:focus:border-indigo-600 dark:focus:ring-indigo-600" +
+        className
+      }
+      ref={localRef}
+    >
+      {children}
+    </textarea>
+  );
+});
