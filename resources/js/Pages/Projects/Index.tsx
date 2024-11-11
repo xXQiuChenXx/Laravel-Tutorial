18691effe0c3c6f9b0f7f328423888e57d84deaf
Qiu Chen <51996408+xXQiuChenXx@users.noreply.github.com>
2024-11-11 21:09:03 +0800

    Update Index.tsx

--- resources/js/Pages/Projects/Index.tsx
+++ resources/js/Pages/Projects/Index.tsx
@@ -1,6 +1,7 @@
 import { ProjectTable } from "@/Components/project_table/DataTable";
+import { Button } from "@/Components/ui/button";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
-import { Head } from "@inertiajs/react";
+import { Head, Link } from "@inertiajs/react";
 
 export default function Index({
   projects,
@@ -10,13 +11,17 @@ export default function Index({
   return (
     <AuthenticatedLayout
       header={
-        <h2 className="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
-          Projects
-        </h2>
+        <div className="flex justify-between items-center">
+          <h2 className="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
+            Projects
+          </h2>
+          <Link href={route("projects.create")}>
+            <Button>Create Project</Button>
+          </Link>
+        </div>
       }
     >
       <Head title="Projects" />
-
 
       <ProjectTable data={projects.data} />
     </AuthenticatedLayout>
